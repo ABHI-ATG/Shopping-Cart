04919dab5369acf927bbd2da94bafdf9b9ab2f2c
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-07-04 18:03:15 +0530

    changes

--- server/App.js
+++ server/App.js
@@ -12,7 +12,7 @@ const port=process.env.PORT;
 app.use(express.json());
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://radiant-pudding-081ed3.netlify.app/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
