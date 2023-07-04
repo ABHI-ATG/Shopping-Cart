b5b6add7e3e8835033955046182b2207b10c2832
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-07-04 17:09:44 +0530

    changes

--- client/src/components/url.js
+++ client/src/components/url.js
@@ -1,2 +1,4 @@
-const url='https://shopping-cart-n8rq.onrender.com/'
+// const url='https://shopping-cart-n8rq.onrender.com/'
+const url='https://shopping-cart-bie3euk4d-abhi-atg.vercel.app/'
+
 export default url;
